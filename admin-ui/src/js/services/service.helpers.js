5b1011f724a83c1d51cf875b4ce807bd684f0d35
PA Nilsson <pa@openproducts.se>
2014-08-18 09:12:36 +0200

    Added validation on username input

--- admin-ui/src/js/services/service.helpers.js
+++ admin-ui/src/js/services/service.helpers.js
@@ -76,7 +76,7 @@ opiaServices.factory('Helpers', ['_',function(_){
   // Regular Expressions
   helpers.regexIP = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   helpers.regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-
+  helpers.regexUsername = /^[a-z]+$/;
 
   return helpers;
 }]);
