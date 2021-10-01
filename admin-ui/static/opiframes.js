163c58ca9bf2c784c14d24de41bcafeb1c2a4856
Tor Krill <tor@openproducts.se>
2021-10-01 15:04:54 +0200

    More refactoring
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- admin-ui/static/opiframes.js
+++ admin-ui/static/opiframes.js
@@ -5,13 +5,15 @@ FramesLoaded['frame_admin'] = true;
 var activeFrame = "frame_admin";
 
 // Pointer into FrameOrder
+/*
 var currentFrame = 0;
 var FrameOrder = [
 	"frame_admin",
 	"frame_mail",
 	"frame_nc"
 ];
-
+*/
+/*
 var FrameSrc = {
 	frame_admin	:	"/admin/admin.php",
 	frame_mail      :	"/mail/index.php",
@@ -23,25 +25,97 @@ var FrameSrcDefaultApp = {
 	frame_mail      :	"",
 	frame_nc	:	"/apps/files"
 }
+*/
 
-var current_user;
-var cookie;
+//var current_user;
+//var cookie;
 var baseurl = "/admin/apps.php";
 var DEBUG = true;
 
+
+function debug_log(msg) {
+	if (DEBUG) console.log(msg);
+}
+
+// Global objects
+var menu;
+var apps;
+var store;
+
+/*
+ * Localstorage wrapper to store objects as stringified json
+ * all keys are prefixed with a "namespace" prefix
+ */
+class Storage
+{
+	constructor(prefix)
+	{
+		this.prefix = prefix+"_";
+	}
+
+
+	initialize(obj)
+	{
+		for( const key in obj)
+		{
+			this.set(key,obj[key]);
+		}
+	}
+
+	get(key)
+	{
+		if( localStorage.hasOwnProperty(this.prefix+key) )
+		{
+			return JSON.parse(localStorage.getItem(this.prefix+key))
+		}
+		return null;
+	}
+
+	set(key, value)
+	{
+		localStorage.setItem(this.prefix+key, JSON.stringify(value));
+	}
+
+	has(key)
+	{
+		return localStorage.hasOwnProperty(this.prefix+key);
+	}
+}
+
+/*
+ * Base class for web-apps
+ * wraps an iframe and manage an app running within.
+ */
 class WebApp
 {
 	constructor(name)
 	{
 		this.name = name;
-		this.frame = null;
+		this.frame = document.getElementById(this.name);
 		this.page = null;
 		this.waitlogin = false;
 		this.waitlogout = false;
 		this.token = "";
 		this.src = "";
 		this.loaded = false;
-		debug_log(this.name + " class created");
+		this.log("class created");
+		this.loggedin = false;
+	}
+
+	show()
+	{
+		this.log("Show");
+		if( ! this.loaded )
+		{
+			this.log("Show called before page loaded!");
+		}
+		this.frame.classList.add("z0");
+	}
+
+	hide()
+	{
+		this.log("Hide");
+		this.frame.classList.remove("z0");
 	}
 
 	log(msg)
@@ -52,11 +126,18 @@ class WebApp
 	login()
 	{
 		this.log("login");
+		this.loggedin = true;
+	}
+
+	isLoggedIn()
+	{
+		return this.loggedin;
 	}
 
 	logout()
 	{
 		this.log("logout");
+		this.loggedin = false;
 	}
 
 	load()
@@ -67,11 +148,16 @@ class WebApp
 	onload()
 	{
 		this.log("onload");
-		this.frame = document.getElementById(this.name);
 		this.page = this.frame.contentWindow.document;
+
+		$(this.page).contents().find("body").click(function() {
+			// any click anywhere should close the OP-menu.
+			menu.close();
+		});
+		
 		this.loaded = true;
-		debug_log(this.page);
-		this.log("onload page dumped");
+		//debug_log(this.page);
+		//this.log("onload page dumped");
 	}
 
 	static frameToApp(frame)
@@ -83,13 +169,45 @@ class WebApp
 	{
 		return "frame_"+app;
 	}
+
+	static fromID(id)
+	{
+		return apps[id];
+	}
+
+	static fromFrame(frame)
+	{
+		return apps[WebApp.frameToApp(frame)];
+	}
+
+	static hideAll()
+	{
+		for( const app in apps)
+		{
+			apps[app].hide();
+		}
+	}
 }
 
+class LoadingApp extends WebApp
+{
+	constructor()
+	{
+		super("frame_loading");
+		$(this.frame).on("load", this.onload.bind(this));
+		this.src = "loading.php";
+	}
+}
+
+/*
+ * RoundCube Webapp
+ */
 class RCApp extends WebApp
 {
 	constructor()
 	{
 		super("frame_mail");
+		$(this.frame).on("load", this.onload.bind(this));
 		this.src = "/mail/index.php";
 	}
 
@@ -97,7 +215,7 @@ class RCApp extends WebApp
 	{
 		super.onload();
 		//debug_log($(this.page).contents());
-		var token = $(this.page).contents().find("input[name=_token]").val();
+		let token = $(this.page).contents().find("input[name=_token]").val();
 		if( token != undefined )
 		{
 			this.token = token;
@@ -176,10 +294,19 @@ class RCApp extends WebApp
 
 	}
 
+	isLoggedIn()
+	{
+		if( this.page && this.page.defaultView.rcmail.env.hasOwnProperty("user_id") )
+		{
+			return true;
+		}
+		return false;
+	}
+
 	static getToken(data) {
-		var token_pattern = /request_token\"\s*\:\s*\"(\w+)\"/;
-		var match = token_pattern.exec(data);
-		var token;
+		let token_pattern = /request_token\"\s*\:\s*\"(\w+)\"/;
+		let match = token_pattern.exec(data);
+		let token;
 		try
 		{
 			token = match[1];
@@ -193,12 +320,17 @@ class RCApp extends WebApp
 	}
 }
 
+/*
+ * Nextcloud webapp
+ */
 class NCApp extends WebApp
 {
 	constructor()
 	{
 		super("frame_nc");
+		$(this.frame).on("load", this.onload.bind(this));
 		this.src = "/nc/index.php";
+		this.current = "";
 	}
 
 	onload()
@@ -207,18 +339,18 @@ class NCApp extends WebApp
 		this.token = $(this.page).contents().find("head").attr("data-requesttoken");
 		this.log("Token: "+ this.token);
 		debug_log(this.page);
-		var isrc = $(this.page).attr("URL");
-		this.log("Src: "+isrc);
-		
-		var app = isrc.substr(isrc.lastIndexOf('/')+1);
-		this.log("App: "+app);
-		
-		if( app != "loading.php" )
-		{
-			$(this.page).contents().find("html").addClass("app_"+app);
-			update_nav(".nav_button[data-app='"+app+"']");
-		}
+
+		let isrc = $(this.page).attr("URL");
+		this.current = isrc.substr(isrc.lastIndexOf('/')+1);
 	}
+
+	load(sub="")
+	{
+		this.log("load: "+sub+" ("+this.current+")");
+		this.frame.src = this.src + "/apps/"+sub;
+		this.current = sub;
+	}
+
 
 	login(args)
 	{
@@ -239,6 +371,7 @@ class NCApp extends WebApp
 				debug_log("NC login request succeded");
 				//debug_log(response);
 				this.waitlogin = false;
+				this.loggedin = true;
 				load_nextframe();
 			})
 		.fail( function()
@@ -249,11 +382,26 @@ class NCApp extends WebApp
 		debug_log("login completed");
 	}
 
+	isLoggedIn()
+	{
+		if( this.page && $(this.page).contents().find("head").attr("data-user"))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	getUser()
+	{
+		return $(this.page).contents().find("head").attr("data-user");
+	}
+
+
 	logout(timeout = 0, url = "", callback, cb_args)
 	{
 		super.logout();
 		let logout_url = "/nc/index.php/logout?requesttoken="+encodeURIComponent(this.token);
-		debug_log("URL" + logout_url);
+		//debug_log("URL" + logout_url);
 
 		$.ajax(	{
 				url: logout_url,
@@ -264,15 +412,16 @@ class NCApp extends WebApp
 			{
 				//debug_log(stat);
 				//debug_log(xhr);
-				debug_log("NC logout done");
+				this.log("NC logout done");
+				this.loggedin = false;
 			})
 		.fail( function()
 			{
-				debug_log("NC logout failed");
+				this.log("NC logout failed");
 			})
 		.always( function()
 			{
-				debug_log("Running NC logout 'always'");
+				this.log("Running NC logout 'always'");
 				if( timeout && url )
 				{
 					this.waitlogout = false;
@@ -281,7 +430,7 @@ class NCApp extends WebApp
 
 				if( typeof callback != "undefined")
 				{
-					debug_log("NC logout running 'callback'");
+					this.log("NC logout running 'callback'");
 					callback(cb_args);
 				}
 			});
@@ -289,12 +438,35 @@ class NCApp extends WebApp
 
 }
 
+/*
+ * Admin app
+ */
 class ADMApp extends WebApp
 {
 	constructor()
 	{
 		super("frame_admin");
+		$(this.frame).on("load", this.onload.bind(this));
 		this.src = "/admin/admin.php";
+	}
+
+	isLoggedIn()
+	{
+		if( this.page && this.page.getElementById("admin"))
+		{
+			return this.page.defaultView.angular.element(this.page.getElementById("admin")).scope().user.isLogged();
+		}
+		return false;
+	}
+
+
+	getUser()
+	{
+		if( this.page && this.page.getElementById("admin"))
+		{
+			return this.page.defaultView.angular.element(this.page.getElementById("admin")).scope().user.username;
+		}
+		return "";
 	}
 
 	// Login provided by opiapp which calls into opiframes
@@ -329,58 +501,61 @@ class ADMApp extends WebApp
 	}
 }
 
+/*
+ * Class coordinating app visualizing and synchronization
+ */
+class AppManager
+{
+	constructor()
+	{
+		this.log("Constructed");
+	}
 
-function debug_log(msg) {
-	if (DEBUG) console.log(msg);
+	log(msg)
+	{
+		debug_log("AppMGR: "+msg);
+	}
 }
+
 
 // Called from opiadmin app upon login
 function set_name(name) {
 	$("#current_user").text(name);
-	current_user = name;
-	debug_log("Trying to read cookie");
-	if($.cookie(current_user)) {
-		cookie = $.parseJSON($.cookie(current_user));
+	store.set("user",name);
+	//current_user = name;
 
-		
-		debug_log("Cookie App: "+cookie.current_app);
-		debug_log("Cookie Frame: "+cookie.current_frame);
-		// Update menu with user stored app
-		if (cookie.current_app)
-		{
-			// Select element by app-assigned "data-app" attribute
-			$(".nav_button[data-app='"+cookie.current_app+"']").addClass("active");
-		} 
-		else 
-		{
-			// Select element by target frame
-			$(".nav_button[target='"+cookie.current_frame+"']").addClass("active");	
-		}
+	debug_log("Read config");
+	debug_log("Config App: "+store.get("app"));
+	debug_log("Config Frame: "+store.get("frame"));
+	
+	// Update menu with user stored app
+	if (store.get("app"))
+	{
+		// Select element by app-assigned "data-app" attribute
+		$(".nav_button[data-app='"+store.get("app")+"']").addClass("active");
+	} 
+	else 
+	{
+		// Select element by target frame
+		$(".nav_button[target='"+store.get("frame")+"']").addClass("active");	
 	}
-	if( cookie && (FrameOrder.indexOf(cookie.current_frame) > 0) ) {
-		// admin_frame is on index '0'
-		activeFrame = cookie.current_frame;
-		// reorder load sequence
-		FrameOrder.splice(FrameOrder.indexOf(activeFrame),1);
-		FrameOrder.splice(1,0,activeFrame);
-	} else {
-		activeFrame = "frame_admin";
-	}
-
-
 }
+
 function load_frame(id) {
-	if (cookie) {
-		if (cookie.current_app) {
-			FrameSrc[cookie.current_frame] = FrameSrc[cookie.current_frame]+"/apps/"+cookie.current_app;
-		} else {
-			FrameSrc[cookie.current_frame] = FrameSrc[cookie.current_frame]+FrameSrcDefaultApp[cookie.current_frame];
-		}
+
+	let app = apps[WebApp.frameToApp(id)];
+	let subapp = "";
+
+	if( store.get("app") )
+	{
+		subapp = store.get("app");;
 	}
-	debug_log("Start Frameload: "+FrameSrc[id] + " id: " + id);
-	$("#"+id).attr('src',FrameSrc[id]);
+	debug_log("load_frame:"+id+ " app " + subapp);
+
+	app.load(subapp);
 }
 
+// Called externally from opiadmin
 function login(args) {
 	// called when admin UI has verified login, pass the same to the owncloud and roundcube
 	debug_log("Login");
@@ -395,8 +570,10 @@ function app_login(args)
 {
 	debug_log("APP Login");
 
-	apps.mail.login(args);
-	apps.nc.login(args);
+	for( const app in apps)
+	{
+		apps[app].login(args);
+	}
 }
 
 function set_url(url) 
@@ -410,16 +587,17 @@ function load_nextframe()
 
 	debug_log("load_nextframe");
 	menu.show();
-	view_frame(activeFrame);
+	view_frame(store.get("frame"));
 	
 	if(apps.nc.waitlogin || apps.mail.waitlogin) {
 		debug_log("Apps not loaded yet: " + apps.nc.waitlogin + " " + apps.mail.waitlogin);
 		return false;
 	}
-
+/*
 	currentFrame++;
 	debug_log("Current Frame: " + currentFrame);
 	load_frame(FrameOrder[currentFrame]);
+*/
 }
 
 function redirect(timeout,url)
@@ -476,46 +654,32 @@ function logout(timeout,url)
 	menu.hide();
 
 	//debug_log("NC token: " + apps.nc.token);
-
-	apps.nc.logout(timeout, url);
-	apps.mail.logout(timeout, url);
-	apps.admin.logout(timeout, url);
+	for(const app in apps)
+	{
+		apps[app].logout(timeout, url);
+	}
 }
 
 function view_frame(Frame)
 {
-
-	$(".subpage").removeClass("z0");
-	
-	var app = WebApp.frameToApp(Frame);
+	WebApp.hideAll();
+	let app = WebApp.frameToApp(Frame);
 
 	debug_log("view_frame: "+Frame+" app "+app);
 	if( apps.hasOwnProperty(app) && apps[app].loaded )
 	{
-		$("#"+Frame).addClass("z0");
+		apps[app].show();
 	} else {
-		$("#frame_loading").addClass("z0");
+		debug_log("Not loaded? "+ apps[app].loaded);
+		apps["loading"].show();
 	}
-
-	/*
-	if(FramesLoaded[Frame]) 
-	{
-		$("#"+Frame).addClass("z0");
-	} else {
-		$("#frame_loading").addClass("z0");
-	}
-	*/
 }
 
 function set_frame(activeFrame,app="") {
-	// Todo: move to local storage, as menu uses.
 	location.hash = activeFrame;
-	debug_log("Setting cookie with current Frame: "+activeFrame+" and current App: "+app);
-	if (app) {
-		$.cookie(current_user,'{"current_frame" : "'+activeFrame+'","current_app" : "'+app+'"}');			
-	} else {
-		$.cookie(current_user,'{"current_frame" : "'+activeFrame+'"}');
-	}
+	debug_log("Update config, current Frame: "+activeFrame+", App: "+app);
+	store.set("frame",activeFrame);
+	store.set("app", app);
 
 	view_frame(activeFrame);
 }
@@ -561,63 +725,36 @@ function update_nav(app) {
 
 }
 
-var menu;
-var apps =
-	{
-		'admin' : new ADMApp(),
-		'mail' : new RCApp(),
-		'nc' : new NCApp()
-	};
-
 $(document).ready(function() {
 
 	menu = new Menu($("#nav_line"));
 	menu.hide();
+	
+	store = new Storage("opiadmin");
+
+	if( ! store.has("user") )
+	{
+		debug_log("Initialize config");
+		store.initialize( {
+			"user": "",
+			"frame": "admin",
+			"app": ""
+		});
+	}
+
+	apps =
+	{
+		'admin' : new ADMApp(),
+		'mail' : new RCApp(),
+		'nc' : new NCApp(),
+		'loading': new LoadingApp()
+	};
+
+	apps.mail.load();
+	apps.nc.load();
+
 	setTitle();
 
-	debug_log("Starting load");
-
-	$("#frame_admin").on("load", apps.admin.onload.bind(apps.admin));
-	$("#frame_mail").on("load", apps.mail.onload.bind(apps.mail));
-	$("#frame_nc").on("load", apps.nc.onload.bind(apps.nc));
-
-	$(".subpage").on("load", function() {
-		debug_log("Loading: "+$(this).attr("id"));
-
-		$(this).contents().find("body").click(function() {
-			// any click anywhere should close the OP-menu.
-			menu.close();
-		});
-/*
-		if(!FramesLoaded[FrameOrder[currentFrame]])
-		{
-			FramesLoaded[FrameOrder[currentFrame]] = true;
-		
-			debug_log("Current frame: "+currentFrame);
-			debug_log("Loaded frame: "+FrameOrder[currentFrame]);
-			
-			if(activeFrame == FrameOrder[currentFrame])
-			{
-				// the frame is active, so hide the loader page and show the real one.
-				$("#frame_loading").removeClass("z0");
-				$(this).addClass("z0");
-				
-			}
-
-			while (FramesLoaded[FrameOrder[currentFrame]] && (currentFrame < FrameOrder.length))
-			{
-				// advance pointer if the frame is alreay loaded.
-				currentFrame++;
-			}
-
-			if(currentFrame < FrameOrder.length)
-			{
-				load_frame(FrameOrder[currentFrame]);
-			}
-		}
-*/
-	});
-	
 	$(".nav_button").click(function() {
 
 		debug_log("Calling setframe from navbutton");
@@ -626,8 +763,7 @@ $(document).ready(function() {
 		update_nav(this);
 
 	});
-	
-	
+
 	$("#op_nav button").hover(function() {
 		$(this).children().toggleClass("hover");
 	});		
@@ -636,6 +772,7 @@ $(document).ready(function() {
 		e.preventDefault();
 		icon_logout(0.1,"/admin");
 	});
+
 	$("#btn_logout_confirm").on('keydown',function(e){
 		if(e.which == 27) {
 			// hide dialog if 'esc' is pressed
@@ -650,7 +787,7 @@ $(document).ready(function() {
 			set_url(baseurl);
 		} else {
 			pattern = /\/apps\/(\w+)/;
-			var app = pattern.exec(target_src);
+			let app = pattern.exec(target_src);
 			if (app != null) {
 				debug_log("HASH CHANGE APP: " + app[1]);
 				set_frame(frame,app[1]);
